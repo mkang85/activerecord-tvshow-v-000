c7ec974c3b1b8d4e239bbb1ea8ee188a2b2c368c
Michael Kang <michaelkang85@gmail.com>
2018-04-15 19:07:32 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -7,4 +7,17 @@ class Show < ActiveRecord::Base
     x = self.highest_rating
     Show.find_by(rating: x)
   end
+
+  def self.lowest_rating
+    Show.minimum(:rating)
+  end
+
+  def self.least_popular_show
+    x = self.least_popular_show
+    Show.find_by(rating: x)
+  end
+
+  def self.ratings_sum
+    Show.sum(:rating)
+  end
 end
